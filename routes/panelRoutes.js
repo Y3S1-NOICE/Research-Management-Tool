4f104e9fc5f3f338474c414dcc1480fa0386b151
amanthajayathilake <amanthajayathilake@gmail.com>
2022-04-19 19:16:25 +0530

    changes

--- routes/panelRoutes.js
+++ routes/panelRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { createPanel, getAllPanels, updatePanel, deletePanel } from "../services/panelService.js";
 import { authorize } from "../middleware/auth.js";
-const { STUDENT, ADMIN } = roles;
 import { roles } from "../utils/utilities.js";
+
+const { STUDENT, ADMIN } = roles;
 
 const router = express.Router();
 
 router.post('/', authorize(ADMIN), createPanel);
-router.get('/', authorize(ADMIN), getAllPanels);
+router.get('/', getAllPanels);
 router.put('/:id', authorize(ADMIN), updatePanel);
 router.delete('/:id', authorize(ADMIN), deletePanel);
 
