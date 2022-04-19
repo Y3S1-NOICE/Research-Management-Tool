16266479f0b07435fcee5adfa46194241a9776f4
amanthajayathilake <amanthajayathilake@gmail.com>
2022-04-19 11:26:17 +0530

    Authorization

--- routes/panelRoutes.js
+++ routes/panelRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { createPanel, getAllPanels, updatePanel, deletePanel } from "../services/panelService.js";
+import { authorize } from "../middleware/auth.js";
+const { STUDENT, ADMIN } = roles;
+import { roles } from "../utils/utilities.js";
 
 const router = express.Router();
 
-router.post('/', createPanel);
-router.get('/', getAllPanels);
-router.put('/:id', updatePanel);
-router.delete('/:id', deletePanel);
+router.post('/', authorize(ADMIN), createPanel);
+router.get('/', authorize(ADMIN), getAllPanels);
+router.put('/:id', authorize(ADMIN), updatePanel);
+router.delete('/:id', authorize(ADMIN), deletePanel);
 
 export default router;
