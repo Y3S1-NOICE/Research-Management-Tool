bffc7791df19e02423fed1809c4752f619f793d9
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-04-23 19:57:18 +0530

    S3 bucket created

---
+++ aws-s3/s3.js
@@ -0,0 +1,53 @@
+import fs from "fs";
+import S3 from 'aws-sdk/clients/s3.js';
+import dotenv from "dotenv";
+dotenv.config();
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+// const bucketName = 'rmt-bucket-1.0v'
+// const region = 'us-east-1'
+// const accessKeyId = 'AKIAXGEKLOVCDR6CWBH2'
+// const secretAccessKey = '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'
+
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey
+})
+
+// uploads a file to s3
+const upload = (folder, file) => {
+  const fileStream = fs.createReadStream(file.path);
+  const params = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: folder+'/'+file.originalname
+  }
+  return s3.upload(params).promise()
+}
+
+// retrieve files meta-data from s3
+const getFilesMetaData = (folderName) => {
+  const params = {
+    Bucket: bucketName,
+    Delimiter: '/',
+    Prefix: folderName+'/'
+  }
+  return s3.listObjects(params).promise();
+}
+
+// downloads a file from s3
+function download(fileKey) {
+  const params = {
+    Key: fileKey,
+    Bucket: bucketName
+  }
+  return s3.getObject(params).createReadStream()
+}
+
+export { upload, download, getFilesMetaData };
