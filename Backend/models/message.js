49d24393ae07394007dc5a654fed827069c78b06
amanthajayathilake <amanthajayathilake@gmail.com>
2022-05-01 18:21:02 +0530

    model and service changes

--- Backend/models/message.js
+++ Backend/models/message.js
@@ -8,14 +8,7 @@ const messageSchema = new mongoose.Schema({
         },
         content: {
             type: String,
-            trim: true, 
             required: true,
-        },
-        pic: {
-            type: String,
-            required: false,
-            // default:
-            //   "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
         },
         sender: {
             type: String, 
--- Backend/routes/chatGroupRoutes.js
+++ Backend/routes/chatGroupRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorize } from "../middleware/auth.js";
-import { createChatGroup, getAllChatGroups, getChatGroup, updateChatGroupDetails, deleteChatGroup } from "../services/chatGroupService.js";
+import { createChatGroup, getAllChatGroups, getChatGroup, updateChatGroupDetails, deleteChatGroup, sendMessages } from "../services/chatGroupService.js";
 import { roles } from "../utils/utilities.js";
 const { STUDENT, ADMIN, PANEL_MEMBER, SUPERVISOR, GROUP_LEADER } = roles;
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post('/', authorize(ADMIN, GROUP_LEADER, SUPERVISOR), createChatGroup);
 router.get('/', authorize(ADMIN, SUPERVISOR), getAllChatGroups);
 router.get('/', authorize(ADMIN, STUDENT, SUPERVISOR, PANEL_MEMBER, GROUP_LEADER), getChatGroup);
+router.put('/:id/sendMessage', authorize(ADMIN, STUDENT, SUPERVISOR, PANEL_MEMBER, GROUP_LEADER), sendMessages);
 router.put('/:id', authorize(ADMIN, GROUP_LEADER, PANEL_MEMBER, SUPERVISOR), updateChatGroupDetails);
 router.delete('/:id', authorize(ADMIN, GROUP_LEADER, SUPERVISOR), deleteChatGroup);
 
