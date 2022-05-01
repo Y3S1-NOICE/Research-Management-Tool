edf55a1c4be5b876174fb601a128d04ecacba841
amanthajayathilake <amanthajayathilake@gmail.com>
2022-05-01 18:32:05 +0530

    refactoring

--- Backend/routes/chatGroupRoutes.js
+++ Backend/routes/chatGroupRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
 import { authorize } from "../middleware/auth.js";
-import { createChatGroup, getAllChatGroups, getChatGroup, updateChatGroupDetails, deleteChatGroup, sendMessages } from "../services/chatGroupService.js";
+import { 
+    createChatGroup, 
+    getAllChatGroups, 
+    getChatGroup, 
+    updateChatGroupDetails, 
+    deleteChatGroup, 
+    sendMessages 
+} from "../services/chatGroupService.js";
 import { roles } from "../utils/utilities.js";
 const { STUDENT, ADMIN, PANEL_MEMBER, SUPERVISOR, GROUP_LEADER } = roles;
 
