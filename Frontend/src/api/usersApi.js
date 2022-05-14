bae56593bfd10336a2a7068312e8a8710017ee26
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-05-14 10:34:35 +0530

    FR unit tests created

--- Frontend/src/api/usersApi.js
+++ Frontend/src/api/usersApi.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.BASE_URL || 'http://localhost:5000';
-axios.defaults.headers.common['authentication'] = localStorage.getItem('authentication');
+// axios.defaults.baseURL = process.env.BASE_URL || 'http://localhost:5000';
+// axios.defaults.headers.common['authentication'] = localStorage.getItem('authentication');
 
 export const registerUser = (userObj) => axios.post('/users', userObj);
 export const findUsers = (queryParams) => axios.get(`/users?${queryParams}`);
 export const updateUser = (userId, userObj) => axios.put(`/users?id=${userId}`, userObj);
 export const deleteUser = (userId) => axios.delete(`/users?id=${userId}`);
+export function sum(a, b) {
+    return a + b;
+  }
