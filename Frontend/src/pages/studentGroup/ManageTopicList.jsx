ef65cc15678b4f926ac019511bda2e0ca8a40d5a
amanthajayathilake <amanthajayathilake@gmail.com>
2022-05-15 04:49:58 +0530

    Topic list integrated

--- Frontend/src/pages/studentGroup/ManageTopicList.jsx
+++ Frontend/src/pages/studentGroup/ManageTopicList.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Container, Grid, Paper, TablePagination } from '@mui/material';
+import { Paper } from '@mui/material';
 import Button from '@mui/material/Button';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -12,37 +7,53 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import TextField from '@mui/material/TextField';
-import { IconButton } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import CancelIcon from '@mui/icons-material/Cancel';
-import AddIcon from '@mui/icons-material/Add';
 import toast, { Toaster } from 'react-hot-toast';
 import Stack from '@mui/material/Stack';
-import { findUsers } from "../../api/usersApi";
 import { getAuth } from '../../helper/helper.js';
-import { fetchStudentGroup, requestCoSupervisor, requestSupervisor } from '../../api/studentGroupApi';
-import AddTopic from '../../components/manage-topics/AddTopic';
+import { fetchAllStudentGroups } from '../../api/studentGroupApi';
+import EditTopicStatus from '../../components/manage-topics/EditTopicStatus';
+
 
 export default function ManageTopics() {
-    const [group, setGroup] = useState([]);
+    const [groups, setGroups] = useState([]);
     const [editOpen, setEditOpen] = useState(false);
+    const [group, setGroup] = useState({});
 
     useEffect (() =>{
         handleGetGroups();
     },[]);
 
-    const handleGetGroups = async() =>{
-        let id = getAuth().id;
-        console.log(id)
-        try{
-            const res = await fetchStudentGroup(`supervisorId=${id}`);
-            setGroup(res.data.responseData);
-            console.log(res.data.responseData)
-        }catch(error){
-            console.log(error)
-        } 
+    const handleGetGroups = () => {
+        fetchAllStudentGroups().then(res => {
+            res.data.isSuccessful ? setGroups(res.data.responseData) : toast.error('Error Retrieving');
+            //console.log(res.data)
+        }).catch(() => {
+            toast.error('Error!', {
+                position: "top-right",
+                style: {
+                  padding: '16px',
+                  color: 'white',
+                  background: '#FF0000'
+                },
+                iconTheme: {
+                  primary: 'red',
+                  secondary: '#FFFAEE',
+                },
+            });
+        });
     }
+
+    // const handleGetGroups = async() =>{
+    //     let id = getAuth().id;
+    //     console.log(id)
+    //     try{
+    //         const res = await fetchStudentGroup(`supervisorId=${id}`);
+    //         setGroup(res.data.responseData);
+    //         console.log(res.data.responseData)
+    //     }catch(error){
+    //         console.log(error)
+    //     } 
+    // }
 
     const id = getAuth().id;
 
@@ -59,23 +70,25 @@ export default function ManageTopics() {
                         <TableHead>
                             <TableRow>
                             {/* <TableCell>ID</TableCell> */}
-                            <TableCell align="left"><b>Topic</b></TableCell>
+                            <TableCell align="left"><b>Group ID</b></TableCell>
+                            <TableCell align="center"><b>Topic</b></TableCell>
                             <TableCell align="center"><b>Area</b></TableCell>
                             <TableCell align="center"><b>Status</b></TableCell>
                             <TableCell align="center"></TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {group.map((group, index) => (
+                            {groups.map((group, index) => (
                                 group.supervisorId === id && (
                                     <>
                                         <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                            <TableCell component="th" scope="row" align="left">{group.researchTopic.topic}</TableCell>
+                                            <TableCell component="th" scope="row" align="left">{group.id}</TableCell>
+                                            <TableCell component="th" scope="row" align="center">{group.researchTopic.topic}</TableCell>
                                             <TableCell align="center">{group.researchTopic.area}</TableCell>
                                             <TableCell align="center">{group.status}</TableCell>
                                             <TableCell align="center">
                                                 <Stack direction="row" spacing={1}>
-                                                    <Button>Update Status</Button>
+                                                    <Button onClick={() => setEditingStatus(group)}>Update Status</Button>
                                                 </Stack>
                                             </TableCell>
                                         </TableRow>
@@ -89,6 +102,7 @@ export default function ManageTopics() {
             {editOpen && group &&
                 <EditTopicStatus
                     group={group}
+                    id={group.id}
                     setEditOpen={setEditOpen}
                     handleGetGroups={handleGetGroups}
                 />
