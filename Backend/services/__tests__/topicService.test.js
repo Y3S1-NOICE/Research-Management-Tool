ba8cdeeaac7e358c18b68be5bdbc04d970217e5d
Charith Lansakara <lansakaracharith@gmail.com>
2022-05-31 11:45:55 +0530

    topic backend unit test implented

---
+++ Backend/services/__tests__/topicService.test.js
@@ -0,0 +1,32 @@
+import researchTopic from "../../models/researchTopic.js";
+import mockingoose from 'mockingoose';
+
+describe('Unit Test for research topic', () => {
+    it('This test should create a research topic', () => {
+        mockingoose(researchTopic)
+        .toReturn({ 
+            topic: 'ReactJS',
+            area: 'Software Engineering', }, 'save')
+    });
+
+  
+    it('This test should update the research topic', () => {
+      const _researchTopicTest = {
+        id:'G001',
+        topic: 'KoaJS',
+        area: 'Software Engineering',
+      
+      };
+  
+      mockingoose(researchTopic).toReturn(_researchTopicTest, 'update');
+  
+      return researchTopic
+        .update({ topic: 'ReactJS' }) // change the topic
+        .where({ topic: 'KoaJS' })
+        .then(_researchTopicTest => {
+          expect(JSON.parse(JSON.stringify(_researchTopicTest))).toMatchObject(_researchTopicTest);
+        });
+    });
+
+    
+  });
